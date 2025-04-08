01169a4e6b994ba89c100af2df9db58ffc85197c
yassir346 <yassir.mahir98@gmail.com>
2025-04-08 11:22:03 +0100

    Adding handleClick Buttons and functions to change values in React with UseState HOOK

--- react_api/src/home.js
+++ react_api/src/home.js
@@ -1,13 +1,25 @@
+import { useState } from 'react';
+
 const Home = () => {
+    const [name, getName] = useState();
+
+    const handleClick = () => {
+        getName('ME');
+    }
+
+
     return ( 
         <div className="home-container">
-            <h1>My profile</h1>
+            <h1>Hello {name}</h1>
+            <h2>My profile</h2>
             <div className="card" style={{width: "18rem"}}>
                 <img src="..." className="card-img-top" alt="..."></img>
                 <div className="card-body">
                     <p className="card-text">example</p>
                 </div>
             </div>
+            <button onClick = {handleClick}>Button</button>
+            
         </div>
      );
 }
